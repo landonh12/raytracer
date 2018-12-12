8a4abaf5199ed5885cf2cb361e06224eea9d1c7a
Landon Haugh <landon.haugh1@gmail.com>
2018-12-12 00:14:15 -0600

    Triangle class completed, about to implement

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -23,7 +23,7 @@
 #define INFINITY 1e8
 #endif
 
-#define MAX_RAY_DEPTH 5
+#define MAX_RAY_DEPTH 2
 
 float mix(const float &a, const float &b, const float &mix)
 {
@@ -31,14 +31,13 @@ float mix(const float &a, const float &b, const float &mix)
 }
 
 //TODO
-Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir, const std::vector<Sphere> &spheres, const std::vector<Cube> &cubes, const int &depth) {
+Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir, const std::vector<Sphere> &spheres, const int &depth) {
     
-    float dist = INFINITY; // Dist to intersection
+    float dist = INFINITY;       // Distance to intersection
     const Sphere *sphere = NULL; // Create sphere object
-    const Cube *cube = NULL;
-    float t0;
-    float t1;
-    float tc;
+    float t0;                    // t0 sphere intersection
+    float t1;                    // t1 sphere intersection
+    
     // Check each object for intersection
     for(int i = 0; i < spheres.size(); i++) {
         // t0, t1 are intersection points
@@ -46,142 +45,146 @@ Vec3f trace(const Vec3f &rayorig, const Vec3f &raydir, const std::vector<Sphere>
         t1 = INFINITY;
         // Gives t0 and t1 back from intersect function
         if(spheres[i].intersect(rayorig, raydir, t0, t1)) {
+            // If we are already inside the sphere, set t0 to t1.
             if(t0 < 0) {
                 t0 = t1;
             }
+            // if t0 is less than infinity, set distance to t0 and assign sphere object
             if(t0 < dist) {
                 dist = t0;
                 sphere = &spheres[i];
             }
         }
     }
-    // If there's no intersection, then sphere will be NULL. Return a bogus Vec3f.
-    if(!sphere){
-        // Check each object for intersection
-        for(int i = 0; i < cubes.size(); i++) {
-            // t0, t1 are intersection points
-            t0 = INFINITY;
-            t1 = INFINITY;
-            // Gives t0 and t1 back from intersect function
-            if(cubes[i].intersect(rayorig, raydir, tc)) {
-                    dist = tc;
-                    cube = &cubes[i];
-            }
-        }
-        if(!cube) {
-            return Vec3f(0.0, 0.0, 0.0);
-        }
-    }
-    Vec3f sColor = 0; // Surface color of the sphere
-    Vec3f pHit = rayorig + raydir * dist; // Point of intersection with the sphere
-    Vec3f nHit;
+    
+    //if(tc < t0) printf("t0: %f, t1: %f, tc: %f", t0,t1,tc);
+    
+    // If there's no cube intersections, then there's no hit!
     if(!sphere) {
-        nHit = pHit - cube->bounds[0];
-    } else {
-        nHit = pHit - sphere->center; // Normal of the surface of intersection
+        return Vec3f(0.0, 0.0, 0.0);
     }
+    
+    Vec3f sColor = Vec3f(0.0, 0.0, 0.0);  // Surface color
+    Vec3f pHit = rayorig + raydir * dist; // Point of intersection with the sphere
+    Vec3f nHit = pHit - sphere->center;   // Normal of the hit
     nHit.normalize(); // Normalize normal dir
+    
     // START REFLECTION AND REFRACTION
     
+    // Biasing
     float bias = 1e-2;
     
+    // Shouldn't be inside the object at first
     bool inside = false;
+    // If the ray direction * normal is > 0, then we are inside the object
     if(raydir.dot(nHit) > 0) {
         nHit = -nHit;
         inside = true;
     }
+    // If there's a sphere object, and it has transparecny or reflection, start a recursive ray tracing function
     if(sphere && (sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
+        // This is just some fresnel effect stuff, got from the example.
         float facingratio = -raydir.dot(nHit);
         float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
+        
+        // Create reflection vector
         Vec3f reflection = Vec3f(0.0,0.0,0.0);
         if(sphere->reflection) {
+            // Reflection direction is ray direction - normal * 2 * ray direction dot normal
             Vec3f refldir = raydir - nHit * 2 * raydir.dot(nHit);
+            // Normalize reflection direction
             refldir.normalize();
-            reflection = trace(pHit + nHit * bias, refldir, spheres, cubes, depth + 1);
+            // Trace reflection ray. Ray origin is pHit + nHit * bias
+            reflection = trace(pHit + nHit * bias, refldir, spheres, depth + 1);
         }
+        // Create refraction vector
         Vec3f refraction = 0;
         if(sphere->transparency) {
+            // Index of reflection and entering/exiting (inverse of index or reflection)
             float ior = 1.345, eta = (inside) ? ior : 1 / ior;
+            // cosi is inverse normal dot ray direction
             float cosi = -nHit.dot(raydir);
             float k = 1 - eta * eta * (1 - cosi * cosi);
             Vec3f refrdir = raydir * eta + nHit * (eta * cosi - sqrt(k));
             refrdir.normalize();
-            refraction = trace(pHit - nHit * bias, refrdir, spheres, cubes, depth + 1);
+            // Trace refraction ray
+            refraction = trace(pHit - nHit * bias, refrdir, spheres, depth + 1);
         }
         
-        sColor = (reflection * 0.7) + (refraction * (1/fresneleffect) * 0.8 * sphere->transparency);
+        // Create surface color based on refraction/reflection rays
+        sColor = ((reflection * 0.7) * 0.5) + ((refraction * (1/fresneleffect) * 0.8 * sphere->transparency) * 0.5);
         
-    } else {
+    }
     
-        // LIGHTS
-        if(sphere) {
-            for(int i = 0; i < spheres.size(); ++i) {
-                if(spheres[i].emissionColor.x > 0) {
-                    Vec3f trans = 1;
-                    Vec3f lightDir = spheres[i].center - pHit;
-                    lightDir.normalize();
-                    for(int j = 0; j < spheres.size(); ++j) {
-                        if(i != j) {
-                            float t0, t1;
-                            if(spheres[j].intersect(pHit + nHit * bias, lightDir, t0, t1)) {
-                                trans = 0.3;
-                                break;
-                            }
-                        }
+    for(int i = 0; i < spheres.size(); ++i) {
+        if(spheres[i].emissionColor.x > 0) {
+            // If we have a sphere that isn't reflecting or refracting, then shoot rays from sphere to light
+            Vec3f trans = 1;
+            // Calculate direction towards light
+            Vec3f lightDir = spheres[i].center - pHit;
+            lightDir.normalize();
+            // For each sphere that isn't a light
+            for(int j = 0; j < spheres.size(); ++j) {
+                if(i != j) {
+                    float t0, t1;
+                    // Check to see if the ray towards the light intersects any sphere
+                    if(spheres[j].intersect(pHit + nHit * bias, lightDir, t0, t1)) {
+                        // if so, then scale color to a factor of 0.3
+                        trans = 0.3;
+                        break;
                     }
-                    sColor += (sphere->surfaceColor) * (trans) * std::max(float(0), nHit.dot(lightDir)) * (spheres[i].emissionColor);
                 }
             }
-        } else if(cube) {
-            for(int i = 0; i < spheres.size(); ++i) {
-                if(spheres[i].emissionColor.x > 0) {
-                    Vec3f trans = 1;
-                    Vec3f lightDir = spheres[i].center - pHit;
-                    lightDir.normalize();
-                    for(int j = 0; j < cubes.size(); ++j) {
-                        float t0;
-                        if(cubes[j].intersect(pHit + nHit * bias, lightDir, t0)) {
-                            trans = 0;
-                            break;
-                        }
-                    }
-                    printf("%f, ", nHit.x);
-                    sColor += (cube->c) * (trans) * std::max(float(0), nHit.dot(lightDir)) * (spheres[i].emissionColor);
-                }
-            }
+            sColor += (sphere->surfaceColor) * (trans) * std::max(float(0), nHit.dot(lightDir)) * (spheres[i].emissionColor);
+            Vec3f h = (-raydir) + lightDir;
+            Vec3f g;
+            if(h.x < 0) g.x = h.x * -1;
+            else g.x = h.x;
+            if(h.y < 0) g.y = h.y * -1;
+            else g.y = h.y;
+            if(h.z < 0) g.z = h.z * -1;
+            else g.z = h.z;
+            Vec3f u;
+            u.x = h.x / g.x;
+            u.y = h.y / g.y;
+            u.z = h.z / g.z;
+            //printf("%f %f %f\n", u.x, u.y, u.z);
+            sColor += 0.2 * std::max(float(0), nHit.dot(u));
         }
     }
     
-    if(sphere) {
-        return sColor + sphere->emissionColor;
-    } else {
-        return cube->c;
-    }
-    
+    return sColor + sphere->emissionColor;
     
 }
 
 //TODO
-void render(const std::vector<Sphere> &spheres, const std::vector<Cube> &cubes)
+void render(const std::vector<Sphere> &spheres)
 {
     unsigned width = 1920, height = 1080;
     Vec3f *image = new Vec3f[width * height], *pixel = image;
     float invWidth = 1 / float(width), invHeight = 1 / float(height);
-    float fov = 30, aspectratio = width / float(height);
+    float fov = 90, aspectratio = width / float(height);
     float angle = tan(M_PI * 0.5 * fov / 180.);
     // Trace rays
+    Vec3f pix;
     for (unsigned y = 0; y < height; ++y) {
         for (unsigned x = 0; x < width; ++x, ++pixel) {
+            for(int i = -2; i < 3; i++) {
             float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
             float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
-            Vec3f raydir(xx, yy, -1);
+            Vec3f raydir(xx + (0.0001 * i), yy + (0.0001 * i), -1);
             raydir.normalize();
-            *pixel = trace(Vec3f(0), raydir, spheres, cubes, 0);
+            pix = trace(Vec3f(0), raydir, spheres, 0);
+            pix.x = pix.x / 5;
+            pix.y = pix.y / 5;
+            pix.z = pix.z / 5;
+            *pixel += pix;
+            }
         }
     }
     
-    // Save result to a PPM image (keep these flags if you compile under Windows)
-    std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
+    // Save result to a PPM image
+    std::ofstream ofs("./tracedimage.ppm", std::ios::out | std::ios::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for (unsigned i = 0; i < width * height; ++i) {
         ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
@@ -198,17 +201,14 @@ int main(int argc, char **argv)
 {
     srand48(13);
     std::vector<Sphere> spheres;
-    std::vector<Cube> cubes;
     // position, radius, surface color, reflectivity, transparency, emission color
-    spheres.push_back(Sphere(Vec3f( -6.0, 0.0, -30), 2, Vec3f(0.50, 0.50, 0.50), 0.0, 0.0));
-    spheres.push_back(Sphere(Vec3f( 0.0, 0.0, -40), 2, Vec3f(0.50, 0.50, 0.50), 0.5, 0.0));
-    spheres.push_back(Sphere(Vec3f( 4.0, 0.0, -23), 2, Vec3f(0.90, 0.90, 0.90), 0.0, 0.1));
-    spheres.push_back(Sphere(Vec3f( 0.0, -20003, -20), 20000, Vec3f(0.5, 0.2, 0.3), 0, 0.0));
-    spheres.push_back(Sphere(Vec3f( 0.0, 0.0, -20060), 20000, Vec3f(0.10, 0.30, 0.10), 0, 0.0));
-    //cubes.push_back(Cube(Vec3f(-6.0, -3.0, -37), Vec3f(6.0, 3.0, -31), Vec3f(0.50, 0.0, 0.00)));
+    spheres.push_back(Sphere(Vec3f( 0.0, -5, -20), 2, Vec3f(0.30, 0.30, 0.30), 0.01, 0.1));
+    spheres.push_back(Sphere(Vec3f( 4.0, -5, -10), 2, Vec3f(0.30, 0.30, 0.30), 0.0, 0.0));
+    spheres.push_back(Sphere(Vec3f( 0.0, -20007, -20), 20000, Vec3f(0.5, 0.2, 0.3), 0.0, 0.0));
+    spheres.push_back(Sphere(Vec3f( 0.0, 0.0, -20060), 20000, Vec3f(0.10, 0.30, 0.10), 0.0, 0.0));
     // light
-    spheres.push_back(Sphere(Vec3f(3.0,  40, -10),     3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(2)));
-    render(spheres, cubes);
+    spheres.push_back(Sphere(Vec3f(0, 60, 10), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(2)));
+    render(spheres);
     
     return 0;
 }
--- raytracer/shapes.cpp
+++ raytracer/shapes.cpp
@@ -52,72 +52,72 @@ public:
     }
 };
 
-class Ray {
+class Triangle {
     
 public:
-    Ray(const Vec3f &orig, const Vec3f &dir) : orig(orig), dir(dir)
+    Vec3f v0;
+    Vec3f v1;
+    Vec3f v2;
+    Vec3f surfaceColor;
+    
+    Triangle(const Vec3f &v0, const Vec3f &v1, const Vec3f &v2, const Vec3f &surfaceColor) :
+    v0(v0), v1(v1), v2(v2), surfaceColor(surfaceColor) {}
+    
+    bool rayTriangleIntersect(
+                              const Vec3f &orig, const Vec3f &dir,
+                              const Vec3f &v0, const Vec3f &v1, const Vec3f &v2,
+                              float &t, Vec3f &N)
     {
-        invdir.x = 1 / dir.x;
-        invdir.y = 1 / dir.y;
-        invdir.z = 1 / dir.z;
-        sign[0] = (invdir.x < 0);
-        sign[1] = (invdir.y < 0);
-        sign[2] = (invdir.z < 0);
-    }
-    Vec3f orig, dir; // ray orig and dir
-    Vec3f invdir;
-    int sign[3];
-};
-
-class Cube {
-    
-public:
-    
-    Vec3f bounds[2];
-    Vec3f c;
-    Cube(const Vec3f &b0, const Vec3f &b1, const Vec3f &color) {
-        bounds[0] = b0;
-        bounds[1] = b1;
-        c = color;
-    }
-    
-    bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t) const {
+        // compute plane's normal
+        Vec3f v0v1 = v1 - v0;
+        Vec3f v0v2 = v2 - v0;
+        // no need to normalize
+        N = v0v1 * v0v2; // N
+        float area2 = N.length();
         
-        Ray r = Ray(rayorig, raydir);
+        // Step 1: finding P
         
-        float tmin, tmax, tymin, tymax, tzmin, tzmax;
+        // check if ray and plane are parallel ?
+        float NdotRayDirection = N.dot(dir);
+        if (fabs(NdotRayDirection) < 0) // almost 0
+            return false; // they are parallel so they don't intersect !
         
-        tmin = (bounds[r.sign[0]].x - r.orig.x) * r.invdir.x;
-        tmax = (bounds[1-r.sign[0]].x - r.orig.x) * r.invdir.x;
-        tymin = (bounds[r.sign[1]].y - r.orig.y) * r.invdir.y;
-        tymax = (bounds[1-r.sign[1]].y - r.orig.y) * r.invdir.y;
+        // compute d parameter using equation 2
+        float d = N.dot(v0);
         
-        if ((tmin > tymax) || (tymin > tmax))
-            return false;
+        // compute t (equation 3)
+        t = (N.dot(orig) + d) / NdotRayDirection;
+        // check if the triangle is in behind the ray
+        if (t < 0) return false; // the triangle is behind
         
-        if (tymin > tmin)
-            tmin = tymin;
-        if (tymax < tmax)
-            tmax = tymax;
+        // compute the intersection point using equation 1
+        Vec3f dir2 = dir;
+        dir2.x = t * dir.x;
+        dir2.y = t * dir.y;
+        dir2.z = t * dir.z;
+        Vec3f P = orig + dir2;
         
-        tzmin = (bounds[r.sign[2]].z - r.orig.z) * r.invdir.z;
-        tzmax = (bounds[1-r.sign[2]].z - r.orig.z) * r.invdir.z;
+        // Step 2: inside-outside test
+        Vec3f C; // vector perpendicular to triangle's plane
         
-        if ((tmin > tzmax) || (tzmin > tmax))
-            return false;
+        // edge 0
+        Vec3f edge0 = v1 - v0;
+        Vec3f vp0 = P - v0;
+        C = edge0 * vp0;
+        if (N.dot(C) < 0) return false; // P is on the right side
         
-        if (tzmin > tmin)
-            tmin = tzmin;
-        if (tzmax < tmax)
-            tmax = tzmax;
+        // edge 1
+        Vec3f edge1 = v2 - v1;
+        Vec3f vp1 = P - v1;
+        C = edge1 * vp1;
+        if (N.dot(C) < 0)  return false; // P is on the right side
         
-        t = tmin;
+        // edge 2
+        Vec3f edge2 = v0 - v2;
+        Vec3f vp2 = P - v2;
+        C = edge2 * vp2;
+        if (N.dot(C) < 0) return false; // P is on the right side;
         
-        if (t < 0) {
-            t = tmax;
-            if (t < 0) return false;
-        }
-        
-        return true;
+        return true; // this ray hits the triangle
     }
 };
